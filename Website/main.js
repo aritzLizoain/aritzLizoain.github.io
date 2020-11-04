3fed72b583e32f6b4a249e50cbf8f5e1ac1b6432
aritzLizoain <65019180+aritzLizoain@users.noreply.github.com>
2020-11-05 00:04:16 +0100

    Update main.js

--- Website/main.js
+++ Website/main.js
@@ -1,3 +1,4 @@
+// Followed: https://www.youtube.com/watch?v=T7PnWnTgusc&t=1089s&ab_channel=JulioCodes
 $(document).ready(function () {
     $('.menu-toggler').on('click', function (){
         $(this).toggleClass('open');
