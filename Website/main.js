2174af0a9b7ad3f95e1c6607bda05d6f6a1f0006
aritzLizoain <65019180+aritzLizoain@users.noreply.github.com>
2020-11-05 23:58:25 +0100

    Update main.js

--- Website/main.js
+++ Website/main.js
@@ -4,4 +4,27 @@ $(document).ready(function () {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
     });
+
+    $('.top-nav .nav-link').on('click', function (){
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+
+    $('nav a[href*="#"]').on('click', function (){
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        },900);
+    });
+
+    $('#up').on('click', function (){
+        $('html, body').animate({
+            scrollTop: 0
+        },900); 
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1500,
+        once: true
+    });
 });
